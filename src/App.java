589dc756f4320117ba27046afb0b1f2acbb69a2c
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-09-29 09:50:24 -0600

    Aqui corre el programa

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+
+import java.util.ArrayList;
+
+import Vistas.Bienvenida;
+import Vistas.Carrera;
+import Vistas.Ganadores;
+import Vistas.Menu;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+
+        /*var lista=new ArrayList<String>();
+        lista.add("David");
+        lista.add("Elimi");
+        lista.add("Alfredo");
+        lista.add("Amada");
+
+        new Carrera(lista);*/
+
+        //new Menu();
+
+        new Bienvenida().setVisible(true);
+       
+        /*new Carrera(lista);*/
     }
 }
