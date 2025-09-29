2d5f03af00d446bf4c91ce9d9070c87d644507ff
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-09-29 09:58:41 -0600

    Modificaciones no importantes

--- src/App.java
+++ src/App.java
@@ -1,26 +1,8 @@
 
-import java.util.ArrayList;
-
 import Vistas.Bienvenida;
-import Vistas.Carrera;
-import Vistas.Ganadores;
-import Vistas.Menu;
 
 public class App {
     public static void main(String[] args)  {
-
-        /*var lista=new ArrayList<String>();
-        lista.add("David");
-        lista.add("Elimi");
-        lista.add("Alfredo");
-        lista.add("Amada");
-
-        new Carrera(lista);*/
-
-        //new Menu();
-
         new Bienvenida().setVisible(true);
-       
-        /*new Carrera(lista);*/
     }
 }
